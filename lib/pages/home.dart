bde3bede1a840ba71190d844a9d5ae710b767183
ivonaponjarac13 <ivonaponjarac@gmail.com>
2026-01-28 14:17:23 +0100

    Kreiranje pocetne stranice

---
+++ lib/pages/home.dart
@@ -0,0 +1,369 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        // <-- Dodano
+        child: SingleChildScrollView(
+          // <-- Dodano
+          child: Container(
+            padding: const EdgeInsets.only(right: 20, top: 20, left: 20.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xffe3e6ff), Color(0xfff1f3ff), Colors.white],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: const [
+                    Icon(Icons.location_on_outlined, color: Colors.black54),
+                    SizedBox(width: 5),
+                    Text(
+                      "Srbija",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  "Zdravo, Ivona!",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                const Text(
+                  "Postoji 10 zanimljivih događaja\nu tvojoj blizini.",
+                  style: TextStyle(
+                    color: Color(0xff6351ec),
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Container(
+                  margin: EdgeInsets.only(right: 10.0),
+                  padding: const EdgeInsets.only(left: 20.0),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 5.0,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.search_outlined),
+                      border: InputBorder.none,
+                      hintText: "Pretraži događaje",
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  height: 100,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 5.0),
+                        child: Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Container(
+                            width: 110,
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  "images/musicnote.png",
+                                  height: 30,
+                                  width: 30,
+                                  fit: BoxFit.cover,
+                                ),
+                                Text(
+                                  "Koncerti",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 30.0),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 5.0),
+                        child: Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Container(
+                            width: 110,
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  "images/sports.png",
+                                  height: 30,
+                                  width: 30,
+                                  fit: BoxFit.cover,
+                                ),
+                                Text(
+                                  "Utakmice",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 30.0),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 5.0),
+                        child: Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Container(
+                            width: 110,
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  "images/video.png",
+                                  height: 30,
+                                  width: 30,
+                                  fit: BoxFit.cover,
+                                ),
+                                Text(
+                                  "Bioskop",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 30.0),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 5.0),
+                        child: Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Container(
+                            width: 110,
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  "images/theatre.png",
+                                  height: 30,
+                                  width: 30,
+                                  fit: BoxFit.cover,
+                                ),
+                                Text(
+                                  "Pozorište",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Predstojeći događaji",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: Text(
+                        "Vidi sve",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  margin: EdgeInsets.only(right: 0.0, left:0.0),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.asset(
+                          "images/koncert.jpg",
+                          height: 200,
+                          width: MediaQuery.of(context).size.width,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 10.0, top: 10.0),
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Feb\n20",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Koncert Zdravka Čolića",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 21.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: Text(
+                        "30€",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xff6351ec),
+                          fontSize: 21.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ), Row(
+                  children: [
+                    Icon(Icons.location_on),
+                    Text(
+                      "Beogradska Arena",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ]
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
